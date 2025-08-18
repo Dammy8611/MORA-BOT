6816d0cc81fcd8d5a77945206f05c87ac19b6f8e
Dammy8611 <damy84511@gmail.com>
2025-08-18 17:00:16 +0000

    n
    Source commit: b2607e1c8bf59bd0ce20ecd74974d9ab5bd716b6
    
    Auto-sync from private repo

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ module.exports = {
     autoReconnect: true,
     businessProfile: false,
     menuStyle: "default",
-    public: true,
+    public: false,
     autoWelcome: true,
     autoLeave: true,
     autoPromote: true,
