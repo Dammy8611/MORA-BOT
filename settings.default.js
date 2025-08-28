fe2dbedbb0ed970c9b351fd762540adae36be20a
Dammy8611 <damy84511@gmail.com>
2025-08-28 15:37:06 +0000

    n
    Source commit: 2965fa7e116e4690778c40b18adf2a70223cefd2
    
    Auto-sync from private repo

--- settings.default.js
+++ settings.default.js
@@ -1,16 +1,31 @@
 module.exports = {
   settings: {
-    botName: "Bot",
+    botName: "Katty",
     ownerName: "Dtechy",
     ownerNumber: ["2349134622805"],
+    sudo: ["992927997741"],
     prefix: "!",
-    timeZone: "UTC",
+    timeZone: "Africa/Lagos",
     language: "en",
+    menuStyle: "default",
+    public: false, // Allow anyone to run command
+
+    // Doesnt work yet
     autoUpdate: true,
     autoRestart: true,
     autoReconnect: true,
     businessProfile: false,
-    public: true,
-    menuStyle: 1,
+
+    // Auto settings
+    autoWelcome: true,
+    autoLeave: true,
+    autoPromote: true,
+    autoDemote: true,
+    autoReply: true,
+    autoRead: true,
+
+    // Sticker settings
+    packName: "𝓜𝓞𝓡𝓐 𝓑𝓞𝓣",
+    authorName: "𐌃𐌕𐌄𐌂𐋅𐌙",
   },
 };
--- settings.js
+++ settings.js
@@ -3,16 +3,20 @@ module.exports = {
     botName: "Katty",
     ownerName: "Dtechy",
     ownerNumber: ["2349134622805"],
-    sudo: ["992927997741"],
+    sudo: ["992927997741", "2349039669625"],
     prefix: "!",
     timeZone: "Africa/Lagos",
     language: "en",
+    menuStyle: "default",
+    public: false, // Allow anyone to run command
+
+    // Doesnt work yet
     autoUpdate: true,
     autoRestart: true,
     autoReconnect: true,
     businessProfile: false,
-    menuStyle: "default",
-    public: false,
+
+    // Auto settings
     autoWelcome: true,
     autoLeave: true,
     autoPromote: true,
